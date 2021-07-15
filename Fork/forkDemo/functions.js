7208cd65e3e13c7969180c89d253969f9f10cc02
Rahzae Williams <84481655+Rahzae@users.noreply.github.com>
2021-07-15 14:51:22 -0400

    Add files via upload

---
+++ Fork/forkDemo/functions.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// -------- Challenge 1 -------- //
+// put this code block into a function, add a return statement, and call the function
+
+
+// return the petString here
+function bestPet() {
+  let bestPet = 'dog';
+  let petName = 'indiana'
+  let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
+  return petString;
+}
+
+
+// call your function here
+bestPet()
+
+
+// -------- Challenge 2 -------- //
+// put this code block into a function, add a return statement, and call the function
+function pet() {
+  let anotherGreatPet = 'bunny';
+  let anotherPetName = 'opal';
+  let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+}
+
+// return the anotherPetString here
+
+// call your function here
+
+
+
+// -------- Challenge 3 -------- //
+// is there another way to write this function so we can use one function for different pets?
+
+let otherPet = "Cat"
+let otherpet = "Snake"
+function(otherPet, otherPet)
+
+
+
